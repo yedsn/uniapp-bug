4e0e9bee013990ee51959eaceef594f101238a2b
jam <yedsn@163.com>
2020-04-26 21:47:01 +0800

    上传项目

---
+++ components/slot-component/slot-component.vue
@@ -0,0 +1,6 @@
+<template>
+	<view>
+		<slot componentValue="我是组件里的值"/>
+	</view>
+</template>
+
---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		{{pageValue}}
+		{{getMethodValue()}}
+		<slot-component>
+			<template v-slot="{componentValue}">
+				{{pageValue}}
+				{{getMethodValue()}}
+				<view>{{componentValue}}</view>
+			</template>
+		</slot-component>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				pageValue: '我是页面的值'
+			}
+		},
+		methods: {
+			getMethodValue() {
+				return '我是方法返回的值'
+			}
+		}
+	}
+</script>
